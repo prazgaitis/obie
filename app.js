419af4fa47d8f1e5bbf3ab36cc28ae04f052ebfe
prazgaitis <prazgaitis@gmail.com>
2020-04-28 18:15:36 -0500

    cleanup

--- app.js
+++ app.js
@@ -5,51 +5,53 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 const port = 3000;
-const { handle } = require("./search");
 
 // global var to store our coverage objects
 let allCarriers = [];
 
 const SheetsHandler = require("./SheetsHandler");
+const { handle } = require("./search");
 let sheets = new SheetsHandler();
 
-(async () => {
-  app.get("/google/redirect", (req, res) => {
-    res.send(req.query.code);
-  });
+// Routes
+app.get("/google/redirect", (req, res) => {
+  res.send(req.query.code);
+});
 
-  // GET route to view all carrier preferences
-  app.get("/carriers", (req, res) => {
-    try {
-      res.send(allCarriers);
-    } catch (e) {
-      res.status(500).send(`Whoops: ${e}`);
-    }
-  });
+// GET route to view all carrier preferences
+app.get("/carriers", (req, res) => {
+  try {
+    res.send(allCarriers);
+  } catch (e) {
+    res.status(500).send(`Whoops: ${e}`);
+  }
+});
 
-  app.post("/search", (req, res) => {
-    const { carriers, error } = handle(req.body, allCarriers);
+app.post("/search", (req, res) => {
+  const { carriers, error } = handle(req.body, allCarriers);
 
-    if (error) {
-      res.status(500).send({ error });
+  if (error) {
+    res.status(500).send({ error });
+  } else {
+    if (carriers.length === 0) {
+      res.status(404).send({ carriers: [], error: "No carriers found" });
     } else {
-      if (carriers.length === 0) {
-        res.status(404).send({ carriers: [], error: "No carriers found" });
-      } else {
-        res.status(200).send({ carriers });
-      }
+      res.status(200).send({ carriers });
     }
-  });
+  }
+});
+
+(async () => {
   app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port} 🚀`)
   );
 
   // Do setup after app starts, because we need the google redirect path
-  // to accept requests
+  // to accept requests before this kicks off
   sheets
     .init()
     .then(async () => {
-      console.log("importing data from Google Sheets...");
+      console.log("⏱ importing data from Google Sheets...");
       allCarriers = await sheets.importData();
     })
     .catch((e) => console.error(e));
